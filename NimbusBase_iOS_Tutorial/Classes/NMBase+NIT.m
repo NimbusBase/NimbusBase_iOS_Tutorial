8e473752d10f6549f2beca2c4201e9f1c1272f1a
coppercash <coderdreamer@gmail.com>
2014-02-07 10:01:35 +0800

    -ObjeC to -ObjC

--- NimbusBase_iOS_Tutorial/Classes/NMBase+NIT.m
+++ NimbusBase_iOS_Tutorial/Classes/NMBase+NIT.m
@@ -15,7 +15,6 @@
     static dispatch_once_t once;
     static NMBase *base;
     dispatch_once(&once, ^{
-        
         NSManagedObjectContext *ctx = [(NITAppDelegate *)[[UIApplication sharedApplication] delegate] managedObjectContext];
         base = [[NMBase alloc] initWithMOContext:ctx configs:self.configs];
     });
@@ -23,7 +22,29 @@
 }
 
 + (NSDictionary *)configs{
-    return nil;
+    static NSString *const kAppName = @"YourAppName";
+    return @{
+             NCfgK_Servers: @[
+                     @{
+                         NCfgK_AppName: kAppName,
+                         NCfgK_Cloud: NCfgV_GDrive,
+                         NCfgK_AppID: @"573163675535-5nj0q1t3lspohv5dj9bhg1b70a4ihbqh.apps.googleusercontent.com",
+                         NCfgK_AppSecret: @"Jkkel_Z8XLO8ecND5sCX5uBq",
+                         },
+                     @{
+                         NCfgK_AppName: kAppName,
+                         NCfgK_Cloud: NCfgV_Dropbox,
+                         NCfgK_AppID: @"vl4r226lxo4dok1",
+                         NCfgK_AppSecret: @"b9d86w0aekl18ly",
+                         },
+                     @{
+                         NCfgK_AppName: kAppName,
+                         NCfgK_Cloud: NCfgV_Box,
+                         NCfgK_AppID: @"eaofybj13ha01qnx4cea2auowdnvwnwc",
+                         NCfgK_AppSecret: @"sTARCW3Asza5ZORLst1Ni2GjrhwlR66p",
+                         },
+                     ]
+             };
 }
 
 @end
