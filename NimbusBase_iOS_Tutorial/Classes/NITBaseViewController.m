9683302dcd1d2b52663be7d7948fcf4d42a70770
coppercash <coderdreamer@gmail.com>
2014-09-23 18:50:55 +0800

    Optimize code in Base view

--- NimbusBase_iOS_Tutorial/Classes/NITBaseViewController.m
+++ NimbusBase_iOS_Tutorial/Classes/NITBaseViewController.m
@@ -23,8 +23,8 @@
 #import "NITServerCell.h"
 
 static NSString
-*sCellReuseIDServer = @"S",
-*sCellReuseIDPlayground = @"C";
+*const sCellReuseIDServer = @"S",
+*const sCellReuseIDPlayground = @"C";
 
 @interface NITBaseViewController ()
 <
@@ -34,104 +34,130 @@ NCUControllerDataSource,
 NCUControllerDelegate
 >
 
-@property(nonatomic, weak)UITableView *tableView;
-@property(nonatomic, strong)NSArray *servers;
+@property (nonatomic, weak) UITableView *tableView;
+@property (nonatomic, strong) NSArray *servers;
 
 @end
 
 @implementation NITBaseViewController
 
-- (id)init{
-    if (self = [super init]) {
+- (id)init
+{
+    if (self = [super init])
+    {
         if([UIViewController instancesRespondToSelector:@selector(edgesForExtendedLayout)]) self.edgesForExtendedLayout=UIRectEdgeNone;
     }
     return self;
 }
 
-- (void)loadView{
+- (void)loadView
+{
     [super loadView];
     
     self.tableView = self.tableView;
 }
 
-- (void)viewDidLoad{
+- (void)viewDidLoad
+{
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
     
     self.servers = self.servers;
 }
 
-- (void)viewDidAppear:(BOOL)animated{
+- (void)viewDidAppear:(BOOL)animated
+{
     [super viewDidAppear:animated];
     
     [self.tableView deselectSelectedRowsAnimated:animated];
 }
 
-- (void)didReceiveMemoryWarning{
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-#pragma mark - UITableViewDataSource
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+#pragma mark - Models
+
+- (NSArray *)servers
 {
-    return 2;
+    if (_servers) return _servers;
+    return _servers = [[NMBase sharedBase] servers];
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    switch (section) {
-        case 0:
-            return self.servers.count;
-            break;
-        case 1:
-            return 1;
-            break;
-        default:
-            break;
+- (NSArray *)tableItems
+{
+    return @[
+             self.servers,
+             @[@"Playground"],
+             ];
+}
+
+#pragma mark - Subviews
+
+- (void)configureCell:(UITableViewCell *)cell atIndexPath:(NSIndexPath *)indexPath
+{
+    id item = self.tableItems[indexPath.section][indexPath.row];
+    
+    if ([item isKindOfClass:[NMBServer class]])
+    {
+        ((NITServerCell *)cell).server = item;
     }
-    
-    return 0;
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-    static NSArray *identifiers = nil;
-    if (identifiers == nil) {
-        identifiers = @[sCellReuseIDServer, sCellReuseIDPlayground];
+    else if ([@"Playground" isEqual:item])
+    {
+        cell.textLabel.text = @"Playground";
     }
-    
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifiers[indexPath.section]
-                                                            forIndexPath:indexPath];
-    [self configureCell:cell atIndexPath:indexPath];
-
-    return cell;
-}
-
-#pragma mark - UITableViewDelegate
-
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
-    
+    /* Remove
     switch (indexPath.section) {
         case 0:{
             NMBServer *server = self.servers[indexPath.row];
-            [self pushViewControllerForServer:server];
+            ((NITServerCell *)cell).server = server;
         }break;
-        case 1:{
-            [self pushPlaygroundViewController];
-        }break;
+        case 1:
+            cell.textLabel.text = @"Playground";
+            break;
         default:
             break;
     }
+     */
+}
+
+- (UITableView *)tableView
+{
+    if (!_tableView)
+    {
+        UIView *superview = self.view;
+        
+        UITableView *tableView = [[UITableView alloc] initWithFrame:superview.bounds style:UITableViewStyleGrouped];
+        _tableView = tableView;
+        
+        tableView.delegate = self;
+        tableView.dataSource = self;
+        
+        [tableView registerClass:[NITServerCell class] forCellReuseIdentifier:sCellReuseIDServer];
+        [tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:sCellReuseIDPlayground];
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = NO;
+        [superview addSubview:tableView];
+        
+        [superview addNoMarginConstraintsToSubview:tableView];
+    }
+    
+    return _tableView;
 }
 
 #pragma mark - Actions
 
-- (void)pushViewControllerForServer:(NMBServer *)server{
+- (void)pushViewControllerForServer:(NMBServer *)server
+{
     NITServerViewController *con = [[NITServerViewController alloc] init];
     con.server = server;
     [self.navigationController pushViewController:con animated:YES];
 }
 
-- (void)pushPlaygroundViewController{
+- (void)pushPlaygroundViewController
+{
     NITAppDelegate
     *appDelegate = (NITAppDelegate *)[[UIApplication sharedApplication] delegate];
     
@@ -149,55 +175,84 @@ NCUControllerDelegate
                                          animated:YES];
 }
 
-#pragma mark - Subviews
+#pragma mark - UITableViewDataSource
 
-- (void)configureCell:(UITableViewCell *)cell atIndexPath:(NSIndexPath *)indexPath{
-    switch (indexPath.section) {
-        case 0:{
-            NMBServer *server = self.servers[indexPath.row];
-            ((NITServerCell *)cell).server = server;
-        }break;
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return self.tableItems.count;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    NSArray *subItems = self.tableItems[section];
+    return subItems.count;
+    /* Remove
+    switch (section) {
+        case 0:
+            return self.servers.count;
+            break;
         case 1:
-            cell.textLabel.text = @"Playground";
+            return 1;
             break;
         default:
             break;
     }
+    
+    return 0;
+     */
 }
 
-- (UITableView *)tableView{
-    if (!_tableView) {
-        UIView *superview = self.view;
-        
-        UITableView *tableView = [[UITableView alloc] initWithFrame:superview.bounds style:UITableViewStyleGrouped];
-        _tableView = tableView;
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    /* Remove
+    static NSArray *identifiers = nil;
+    if (identifiers == nil)
+        identifiers = @[sCellReuseIDServer, sCellReuseIDPlayground];
+     */
 
-        tableView.delegate = self;
-        tableView.dataSource = self;
-        
-        [tableView registerClass:[NITServerCell class] forCellReuseIdentifier:sCellReuseIDServer];
-        [tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:sCellReuseIDPlayground];
-
-        tableView.translatesAutoresizingMaskIntoConstraints = NO;
-        [superview addSubview:tableView];
-        
-        [superview addNoMarginConstraintsToSubview:tableView];
-
-    }
     
-    return _tableView;
+    id item = self.tableItems[indexPath.section][indexPath.row];
+    
+    NSString *reuserID = nil;
+    if ([item isKindOfClass:[NMBServer class]])
+        reuserID = sCellReuseIDServer;
+    else if ([@"Playground" isEqual:item])
+        reuserID = sCellReuseIDPlayground;
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:reuserID
+                                                            forIndexPath:indexPath];
+    [self configureCell:cell atIndexPath:indexPath];
+
+    return cell;
 }
 
-#pragma mark - Servers
-- (NSArray *)servers{
-    if (!_servers) {
-        _servers = [[NMBase sharedBase] servers];
-    }
+#pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    id item = self.tableItems[indexPath.section][indexPath.row];
     
-    return _servers;
+    if ([item isKindOfClass:[NMBServer class]])
+        [self pushViewControllerForServer:item];
+    else if ([@"Playground" isEqual:item])
+        [self pushPlaygroundViewController];
+    /*
+    switch (indexPath.section) {
+        case 0:{
+            NMBServer *server = self.servers[indexPath.row];
+            [self pushViewControllerForServer:server];
+        }break;
+        case 1:{
+            [self pushPlaygroundViewController];
+        }break;
+        default:
+            break;
+    }
+     */
 }
 
 #pragma mark - NCUModelViewControllerDataSource
+
 - (NSManagedObjectContext *)managedObjectContextForController:(NCUController *)controller
 {
     NITAppDelegate
@@ -205,9 +260,14 @@ NCUControllerDelegate
     return appDelegate.managedObjectContext;
 }
 
-- (NSArray *)controller:(NCUController *)controller
-sortDecriptorsForEntity:(NSEntityDescription *)entity
+- (NSArray *)controller:(NCUController *)controller sortDecriptorsForEntity:(NSEntityDescription *)entity
 {
+    static NSDictionary *map = nil;
+    if (map == nil)
+        map = @{
+                @"User": @"name",
+                };
+    /* Remove
     NSArray *sortDescriptors = nil;
     NSString *name = entity.name;
     if ([name isEqualToString:@"User"]) {
@@ -219,13 +279,22 @@ sortDecriptorsForEntity:(NSEntityDescription *)entity
     else if ([name isEqualToString:@"Player"]) {
         sortDescriptors = @[[[NSSortDescriptor alloc] initWithKey:@"user.name" ascending:YES]];
     }
-    
-    return sortDescriptors;
+    */
+    return @[
+             [[NSSortDescriptor alloc] initWithKey:map[entity.name]
+                                         ascending:YES],
+             ];
 }
 
-- (NSString *)controller:(NCUController *)viewController
-    keyPathOfObjectTitle:(NSEntityDescription *)entity
+- (NSString *)controller:(NCUController *)viewController keyPathOfObjectTitle:(NSEntityDescription *)entity
 {
+    static NSDictionary *map = nil;
+    if (map == nil)
+        map = @{
+                @"User": @"name",
+                };
+
+    /* Remove
     NSString *keyPath = nil;
     NSString *name = entity.name;
     if ([name isEqualToString:@"User"]) {
@@ -237,8 +306,8 @@ sortDecriptorsForEntity:(NSEntityDescription *)entity
     else if ([name isEqualToString:@"Player"]) {
         keyPath = @"user.name";
     }
-    
-    return keyPath;
+    */
+    return map[entity.name];
 }
 
 @end
